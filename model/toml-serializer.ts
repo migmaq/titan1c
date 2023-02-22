4d6d376f0392af4bef823dd9cbc6f988071c603b
Scott Calway <scott.calway@gmail.com>
2023-02-21 22:02:33 -0500

    toml-serializer updated to output as in sample-data

--- model/toml-serializer.ts
+++ model/toml-serializer.ts
@@ -1,23 +1,73 @@
 import { parse as toml_parse, stringify as toml_stringify } from "https://deno.land/std@0.177.0/encoding/toml.ts";
-import {Entry, OrthoText} from "./entry.ts";
+ import {CEntry as Entry} from "./entry.ts";
 
 
 async function round_trip_test(entry_toml_path: string) {
     const entry_toml_text = await Deno.readTextFile(entry_toml_path);
     const entry = toml_parse(entry_toml_text);
-    console.info(toml_stringify(entry));
+    //console.info(toml_stringify(entry));
     super_toml_stringify(entry);
 }
 
 function super_toml_stringify(data: any): string {
+    // console.info(Object.getOwnPropertyNames((new Entry()).categories[0]));
+    // console.info(typeof(new Entry()).categories[0]);
+    for (const key in data) {    
+        toml_stringify_data("",key,data[key],0,false);
+    }
+    return "";
+}
 
-    for (const key in data) {
-        console.info ("key:",key);
-        const value = data[key];
-        console.info("value:", typeof value);
+function toml_stringify_data(parent_key: string, key: string, data: any,tabs: number,arr: boolean): string {
+    const tab = "   ";
+    const tab_text = tab.repeat(tabs);
+    let type = "";
+    let full_key = key;
+    if (parent_key !=="") { full_key = `${parent_key}.${key}` ; }
+    if (Array.isArray(data)) { type = "array";}
+    else { type = typeof(data); }
+    // console.info(tab_text, "Type:", type);
+    
+    switch (type)
+    {
+        case "object":
+            // console.info(tab_text, "{");
+            for (const curkey in data) {
+                // console.info (tab + tab_text, "key:",key);
+                toml_stringify_data(full_key,curkey, data[curkey],tabs+1,arr);
+            }
+            // console.info(tab_text, "}");
+            break;
+        case "array":
+            if (data.length === 0) {
+                console.info(`${tab.repeat(tabs)}${full_key}= []`);
+            }
+            else {
+                // console.info(tab_text, key, "= [");
+                for (const element of data){
+                    // console.info("\n",tab_text, "[[",key, "]]");
+                    console.info(`\n${tab.repeat(tabs)}[[${full_key}]]`);
+                    toml_stringify_data("",full_key,element,tabs,true);
+                }
+                // console.info(tab_text, "]");
+            }
+            break;
+        case "string":
+            if (arr) {
+                tabs--;
+                full_key = key
+            }
+            console.info(`${tab.repeat(tabs)}${full_key} = ${toml_string(data)}`);
+            break;
+        default:
+            if (arr) {
+                tabs--;
+                full_key = key
+            }
+            console.info(`${tab.repeat(tabs)}${full_key} = ${data}`);
     }
 
-    return "cat";
+    return "";
 }
 
 function toml_entry(entry): string {
@@ -25,6 +75,8 @@ function toml_entry(entry): string {
     toml_key_and_value("_id",entry._id,"");   
     toml_key_and_value("public_id",entry.public_id,"");
     toml_key_and_value("part_of_speech",entry.part_of_speech,"");   
+
+    return "";
     
 }
 
@@ -42,9 +94,9 @@ function toml_string (text:string): string {
     if (typeof text !== 'string')
         throw new Error (`expected sting, got: "${text}"`);
 
-    if (text.indexof("'''") !== -1)
+    if (text.indexOf("'''") !== -1)
         return toml_single_line_string(text);
-    else if (text.indexof("\n") !== -1)
+    else if (text.indexOf("\n") !== -1)
         return toml_multi_line_string(text);
     else
         return toml_single_line_string(text);
@@ -60,7 +112,7 @@ function toml_single_line_string(text:string): string {
 function toml_multi_line_string(text:string): string {
     if (text.indexOf("'''") !== -1)
         throw new Error (`too many quotes :"${text}"`);
-    return "'''\n" + text + "'''";
+    return "'''" + text + "'''";
 }
 
-await round_trip_test("../lexbuilder/entries/a/ajipjutoq/data.toml");
+await round_trip_test("./model/sample-data.toml");
